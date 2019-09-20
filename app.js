32341c660a6193befad8a5060f39c05251bdfdbc
vtakaj <vtakaj@gmail.com>
2019-09-20 18:36:19 +0900

    created tweet model.

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const a = 'hoge'
-const b = 1 + 2
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const app = express()
+app.use(morgan('short'))
 
-console.log(a + b)
+// database
+const dbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/tweet'
+const options = { useNewUrlParser: true }
+if (process.env.MONGODB_ADMIN_NAME) {
+  options.user = process.env.MONGODB_ADMIN_NAME
+  options.pass = process.env.MONGODB_ADMIN_PASS
+  options.auth = { authSource: 'admin' }
+}
+mongoose.connect(dbUrl, options)
+
+// server
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(require('./controllers'))
+
+app.listen(process.env.PORT || 3000, () => {})
